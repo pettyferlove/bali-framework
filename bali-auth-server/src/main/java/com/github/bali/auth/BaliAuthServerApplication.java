c9e38ba256e67dc76caa82badbfda0ae5aa2b3f8
Alex Pettyfer <pettyferlove@live.cn>
2020-12-28 20:50:42 +0800

    :tada: Initial commit.

---
+++ bali-auth-server/src/main/java/com/github/bali/auth/BaliAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.bali.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaliAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaliAuthServerApplication.class, args);
+    }
+
+}
