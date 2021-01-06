d0b2507cacfabaded00cbc972f80a552e7b89593
Pettyfer <pettyferlove@live.cn>
2021-01-06 16:33:44 +0800

    :tada: 移除授权服务器的资源，资源服务器只负责颁发令牌

--- bali-auth-server/src/main/java/com/github/bali/auth/service/impl/BaliUserDetailServiceImpl.java
+++ bali-auth-server/src/main/java/com/github/bali/auth/service/impl/BaliUserDetailServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.bali.auth.service.impl;
 import com.github.bali.auth.constants.SecurityConstant;
 import com.github.bali.auth.service.OAuth2UserDetailsService;
 import com.github.bali.auth.userdetails.BaliUserDetails;
+import com.google.common.base.Preconditions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -29,20 +30,27 @@ public class BaliUserDetailServiceImpl implements OAuth2UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        List<String> roles = new ArrayList<>();
-        roles.add("USER");
-        roles.add("ADMIN");
-        String encode = new MessageDigestPasswordEncoder("SHA-256").encode("123456");
-        return BaliUserDetails.builder()
-                .id(UUID.randomUUID().toString())
-                .username("pettyfer")
-                .password(encode)
-                .status(SecurityConstant.STATUS_NORMAL)
-                .nickname("AlexPettyfer")
-                .roles(roles)
-                .email("pettyferlove@live.cn")
-                .tenant("000000001")
-                .avatar("https://bali-attachment.oss-cn-shanghai.aliyuncs.com/bali/avatar/6fdbd6c29f074e38abe1ad6929351192.png")
-                .build();
+        BaliUserDetails userDetails = null;
+        if ("pettyfer".equals(username)) {
+            List<String> roles = new ArrayList<>();
+            roles.add("USER");
+            roles.add("ADMIN");
+            String encode = new MessageDigestPasswordEncoder("SHA-256").encode("123456");
+            userDetails = BaliUserDetails.builder()
+                    .id(UUID.randomUUID().toString())
+                    .username("pettyfer")
+                    .password(encode)
+                    .status(SecurityConstant.STATUS_NORMAL)
+                    .nickname("AlexPettyfer")
+                    .roles(roles)
+                    .email("pettyferlove@live.cn")
+                    .tenant("000000001")
+                    .avatar("https://bali-attachment.oss-cn-shanghai.aliyuncs.com/bali/avatar/6fdbd6c29f074e38abe1ad6929351192.png")
+                    .build();
+        } else {
+            userDetails = BaliUserDetails.builder().build();
+        }
+        Preconditions.checkNotNull(userDetails.getId(), "没有找到该用户");
+        return userDetails;
     }
 }
