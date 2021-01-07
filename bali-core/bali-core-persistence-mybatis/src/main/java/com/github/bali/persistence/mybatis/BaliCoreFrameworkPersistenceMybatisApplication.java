8e17d816e2445ab929fd7a924431691399617aa6
Pettyfer <pettyferlove@live.cn>
2021-01-07 17:31:38 +0800

    :tada: 修改OAuth2Redis序列化方式

---
+++ bali-core/bali-core-persistence-mybatis/src/main/java/com/github/bali/persistence/mybatis/BaliCoreFrameworkPersistenceMybatisApplication.java
@@ -0,0 +1,16 @@
+package com.github.bali.persistence.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pettyfer
+ */
+@SpringBootApplication
+public class BaliCoreFrameworkPersistenceMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaliCoreFrameworkPersistenceMybatisApplication.class, args);
+    }
+
+}
