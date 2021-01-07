ea98d5ba2953337478f17e569a1b3de9ff5b3a18
Alex Pettyfer <pettyferlove@live.cn>
2021-01-07 21:49:56 +0800

    :tada: Initial commit.

--- bali-auth-server/src/main/java/com/github/bali/auth/BaliAuthServerApplication.java
+++ bali-auth-server/src/main/java/com/github/bali/auth/BaliAuthServerApplication.java
@@ -1,14 +1,24 @@
 package com.github.bali.auth;
 
+import com.github.bali.auth.utils.SecurityUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Petty
  */
 @SpringBootApplication
+@RestController
 public class BaliAuthServerApplication {
+
+    @GetMapping("user")
+    public Object user() {
+        Object principal = SecurityUtil.getAuthentication().getPrincipal();
+        return principal;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BaliAuthServerApplication.class, args);
