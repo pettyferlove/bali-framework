e6706e5febbc155163b97d6172b53da59d204467
Pettyfer <pettyferlove@live.cn>
2021-01-07 17:36:40 +0800

    :tada: 修改OAuth2Redis序列化方式

---
+++ bali-core/bali-core-log/src/main/java/com/github/bali/log/BaliCoreLogApplication.java
@@ -0,0 +1,13 @@
+package com.github.bali.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaliCoreLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaliCoreLogApplication.class, args);
+    }
+
+}
---
+++ bali-core/bali-core-security/src/main/java/com/github/bali/security/BaliCoreSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.github.bali.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaliCoreSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaliCoreSecurityApplication.class, args);
+    }
+
+}
