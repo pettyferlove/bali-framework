6899119671dbf12078eddb9201c10fc5eb332e36
Alex Pettyfer <pettyferlove@live.cn>
2021-01-09 13:50:47 +0800

    :tada: Initial commit.

--- bali-auth-server/src/main/java/com/github/bali/auth/controller/ResourceController.java
+++ bali-auth-server/src/main/java/com/github/bali/auth/controller/ResourceController.java
@@ -2,6 +2,7 @@ package com.github.bali.auth.controller;
 
 import com.github.bali.security.userdetails.BaliUserDetails;
 import com.github.bali.security.utils.SecurityUtil;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/resource")
+@PreAuthorize("#oauth2.hasScope('resource.read')")
 public class ResourceController {
 
 
