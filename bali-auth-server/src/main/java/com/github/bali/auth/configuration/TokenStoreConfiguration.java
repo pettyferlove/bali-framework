7a64fc9810ed2f97131a710e49dd975c3f82f517
Pettyfer <pettyferlove@live.cn>
2021-01-11 17:11:40 +0800

    用户注册接口

--- bali-auth-server/src/main/java/com/github/bali/auth/configuration/TokenStoreConfiguration.java
+++ bali-auth-server/src/main/java/com/github/bali/auth/configuration/TokenStoreConfiguration.java
@@ -1,6 +1,6 @@
 package com.github.bali.auth.configuration;
 
-import com.github.bali.auth.provider.token.JdkSerializationStrategy;
+import com.github.bali.auth.provider.token.FastJsonRedisTokenStoreSerializationStrategy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
@@ -25,7 +24,7 @@ public class TokenStoreConfiguration {
     @ConditionalOnClass(RedisConnectionFactory.class)
     public TokenStore redisTokenStore(RedisConnectionFactory redisConnectionFactory) {
         RedisTokenStore redisTokenStore = new RedisTokenStore(redisConnectionFactory);
-        redisTokenStore.setSerializationStrategy(new JdkSerializationStrategy(new JdkSerializationRedisSerializer(this.getClass().getClassLoader())));
+        redisTokenStore.setSerializationStrategy(new FastJsonRedisTokenStoreSerializationStrategy());
         redisTokenStore.setPrefix("security:oauth2:");
         return redisTokenStore;
     }
--- bali-auth-server/src/main/java/com/github/bali/auth/provider/token/FastJsonRedisTokenStoreSerializationStrategy.java
+++ bali-auth-server/src/main/java/com/github/bali/auth/provider/token/FastJsonRedisTokenStoreSerializationStrategy.java
@@ -1,24 +1,12 @@
 package com.github.bali.auth.provider.token;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.parser.DefaultJSONParser;
-import com.alibaba.fastjson.parser.ParserConfig;
-import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.util.IOUtils;
-import com.alibaba.fastjson.util.TypeUtils;
 import com.google.common.base.Preconditions;
 import org.springframework.data.redis.serializer.SerializationException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.oauth2.common.DefaultOAuth2RefreshToken;
-import org.springframework.security.oauth2.provider.AuthorizationRequest;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStoreSerializationStrategy;
-import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 
-import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -27,45 +15,6 @@ import java.nio.charset.StandardCharsets;
  * @author Petty
  */
 public class FastJsonRedisTokenStoreSerializationStrategy implements RedisTokenStoreSerializationStrategy {
-
-    private static final ParserConfig CONFIG = ParserConfig.getGlobalInstance();
-
-    static {
-        init();
-    }
-
-    protected static void init() {
-        //自定义oauth2序列化：DefaultOAuth2RefreshToken 没有setValue方法，会导致JSON序列化为null
-        CONFIG.setAutoTypeSupport(true);
-        CONFIG.putDeserializer(DefaultOAuth2RefreshToken.class, new DefaultOAuth2RefreshTokenDeserializer());
-        CONFIG.putDeserializer(OAuth2Authentication.class, new OAuth2AuthenticationDeserializer());
-        CONFIG.putDeserializer(AuthorizationRequest.class, new ObjectDeserializer() {
-            @Override
-            public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
-                return null;
-            }
-
-            @Override
-            public int getFastMatchToken() {
-                return 0;
-            }
-        });
-        CONFIG.addAccept("org.springframework.security.oauth2.provider.");
-        CONFIG.addAccept("org.springframework.security.oauth2.provider.client");
-
-        TypeUtils.addMapping("org.springframework.security.oauth2.provider.OAuth2Authentication",
-                OAuth2Authentication.class);
-        TypeUtils.addMapping("org.springframework.security.oauth2.provider.client.BaseClientDetails",
-                BaseClientDetails.class);
-        TypeUtils.addMapping("org.springframework.security.web.savedrequest.DefaultSavedRequest",
-                DefaultSavedRequest.class);
-        TypeUtils.addMapping("org.springframework.security.authentication.InternalAuthenticationServiceException",
-                InternalAuthenticationServiceException.class);
-        TypeUtils.addMapping("org.springframework.security.authentication.BadCredentialsException",
-                BadCredentialsException.class);
-        TypeUtils.addMapping("org.springframework.security.oauth2.provider.AuthorizationRequest",
-                AuthorizationRequest.class);
-    }
 
     @Override
     public <T> T deserialize(byte[] bytes, Class<T> clazz) {
@@ -76,7 +25,7 @@ public class FastJsonRedisTokenStoreSerializationStrategy implements RedisTokenS
         }
 
         try {
-            return JSON.parseObject(new String(bytes, IOUtils.UTF8), clazz, CONFIG);
+            return JSON.parseObject(new String(bytes, IOUtils.UTF8), clazz, FastJsonRedisParserConfig.DEFAULT_CONFIG);
         } catch (Exception ex) {
             throw new SerializationException("Could not serialize: " + ex.getMessage(), ex);
         }
