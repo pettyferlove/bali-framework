077f1f1d7320af7c26832f85abf0eb354367c4fa
Alex Pettyfer <pettyferlove@live.cn>
2021-01-13 19:04:20 +0800

    :tada: Initial commit.

--- bali-auth-server/src/main/resources/db/changelog/init/changelog-init-ddl.mysql.sql
+++ bali-auth-server/src/main/resources/db/changelog/init/changelog-init-ddl.mysql.sql
@@ -1,50 +1,53 @@
 --liquibase formatted sql
 
---changeset Petty:1610268206696-1
+--changeset Petty:1610534973313-1
 CREATE TABLE uc_auth_client_details (id VARCHAR(128) NOT NULL, client_id VARCHAR(32) NOT NULL COMMENT '客户端名称', resource_ids VARCHAR(256) NULL COMMENT '客户端资源集', client_secret VARCHAR(256) NULL COMMENT '客户端密钥', scope VARCHAR(256) NULL COMMENT '域', authorized_grant_types VARCHAR(256) NULL COMMENT '授权模式', web_server_redirect_uri VARCHAR(256) NULL COMMENT 'Web Client回调URL', authorities VARCHAR(256) NULL COMMENT '权限信息', access_token_validity INT NULL COMMENT 'token有效时间，单位毫秒', refresh_token_validity INT NULL COMMENT '刷新token有效时间，单位毫秒', additional_information VARCHAR(4096) NULL COMMENT '附加信息', auto_approve VARCHAR(1000) NULL COMMENT '自动授权', del_flag TINYINT(3) DEFAULT 0 NULL COMMENT '删除标记 0 未删除 1 删除', creator VARCHAR(128) NULL COMMENT '创建人', create_time datetime NULL COMMENT '创建时间', modifier VARCHAR(128) NULL COMMENT '修改人', modify_time datetime NULL COMMENT '修改时间', tenant_id VARCHAR(255) NULL COMMENT '租户ID', CONSTRAINT PK_UC_AUTH_CLIENT_DETAILS PRIMARY KEY (id)) COMMENT='终端信息';
 ALTER TABLE uc_auth_client_details COMMENT = '终端信息';
 
---changeset Petty:1610268206696-2
+--changeset Petty:1610534973313-2
 CREATE TABLE uc_role (id VARCHAR(128) NOT NULL, `role` VARCHAR(128) NOT NULL COMMENT '角色', role_name VARCHAR(128) NOT NULL COMMENT '角色名称', `description` VARCHAR(400) NULL COMMENT '描述', sort SMALLINT NULL COMMENT '排序号', status TINYINT(3) DEFAULT 1 NOT NULL COMMENT '状态 1有效 0无效 默认为1', tenant_id VARCHAR(255) NULL COMMENT '租户ID', del_flag TINYINT(3) UNSIGNED DEFAULT 0 NULL COMMENT '删除标记 0 未删除 1 删除', creator VARCHAR(128) NULL COMMENT '创建人', create_time datetime NULL COMMENT '创建时间', modifier VARCHAR(128) NULL COMMENT '修改人', modify_time datetime NULL COMMENT '修改时间', CONSTRAINT PK_UC_ROLE PRIMARY KEY (id)) COMMENT='用户角色';
 ALTER TABLE uc_role COMMENT = '用户角色';
 
---changeset Petty:1610268206696-3
+--changeset Petty:1610534973313-3
 CREATE TABLE uc_tenant (id VARCHAR(128) DEFAULT '' NOT NULL, tenant_id VARCHAR(128) NOT NULL COMMENT '租户ID', tenant_name VARCHAR(128) NOT NULL COMMENT '租户名', link_man VARCHAR(32) NOT NULL COMMENT '联系人', contact_number VARCHAR(32) NOT NULL COMMENT '联系电话', address VARCHAR(255) NULL COMMENT '地址', remark VARCHAR(255) NULL COMMENT '备注', email VARCHAR(128) NULL COMMENT '电子邮件', status TINYINT(3) DEFAULT 0 NOT NULL COMMENT '是否有效 0 无效 1 有效', del_flag TINYINT(3) UNSIGNED DEFAULT 0 NULL COMMENT '删除标记 0 未删除 1 删除', creator VARCHAR(128) NULL COMMENT '创建人', create_time datetime NULL COMMENT '创建时间', modifier VARCHAR(128) NULL COMMENT '修改人', modify_time datetime NULL COMMENT '修改时间', CONSTRAINT PK_UC_TENANT PRIMARY KEY (id)) COMMENT='租户信息';
 ALTER TABLE uc_tenant COMMENT = '租户信息';
 
---changeset Petty:1610268206696-4
+--changeset Petty:1610534973313-4
 CREATE TABLE uc_user (id VARCHAR(128) DEFAULT '' NOT NULL, login_id VARCHAR(255) NULL COMMENT '用户登录名', password VARCHAR(255) NULL COMMENT '账号密码', tenant_id VARCHAR(255) NULL COMMENT '租户ID', open_id VARCHAR(255) NULL COMMENT '第三方OpenID', union_id VARCHAR(255) NULL COMMENT '第三方组织ID', user_channel VARCHAR(255) DEFAULT 'web' NOT NULL COMMENT '用户所属渠道', status TINYINT(3) DEFAULT 0 NOT NULL COMMENT '是否有效 0：无效 1：有效 8：凭证无效 9：锁定', del_flag TINYINT(3) UNSIGNED DEFAULT 0 NULL COMMENT '删除标记 0 未删除 1 删除', creator VARCHAR(128) NULL COMMENT '创建人', create_time datetime NULL COMMENT '创建时间', modifier VARCHAR(128) NULL COMMENT '修改人', modify_time datetime NULL COMMENT '修改时间', CONSTRAINT PK_UC_USER PRIMARY KEY (id)) COMMENT='用户注册信息';
 ALTER TABLE uc_user COMMENT = '用户注册信息';
 
---changeset Petty:1610268206696-5
-CREATE TABLE uc_user_info (id VARCHAR(128) DEFAULT '' NOT NULL, user_id VARCHAR(128) NOT NULL COMMENT '用户ID', user_name VARCHAR(128) NULL COMMENT '姓名', nick_name VARCHAR(128) NULL COMMENT '用户昵称', user_sex TINYINT(3) NULL COMMENT '性别', user_born datetime NULL COMMENT '生日', user_avatar VARCHAR(128) NULL COMMENT '用户头像', email VARCHAR(128) NULL COMMENT '电子邮件', user_address VARCHAR(400) NULL COMMENT '居住地址', mobile_tel VARCHAR(128) NULL COMMENT '移动电话', phone_tel VARCHAR(96) NULL COMMENT '用户联系电话', user_iden_type VARCHAR(64) NULL COMMENT '用户证件类型', user_iden VARCHAR(128) NULL COMMENT '证件ID', `organization` VARCHAR(255) NULL COMMENT '工作单位', education VARCHAR(255) NULL COMMENT '学历', positional_title VARCHAR(255) NULL COMMENT '职称', political_status VARCHAR(64) NULL COMMENT '政治面貌', graduated_school VARCHAR(255) NULL COMMENT '毕业学校', graduated_year VARCHAR(128) NULL COMMENT '毕业年份', major VARCHAR(128) NULL COMMENT '所学专业', verified TINYINT(3) DEFAULT 0 NULL COMMENT '是否实名认证', region_id VARCHAR(96) NULL COMMENT '行政区划CODE', tenant_id VARCHAR(255) NULL COMMENT '租户ID', del_flag TINYINT(3) UNSIGNED DEFAULT 0 NULL COMMENT '删除标记 0 未删除 1 删除', creator VARCHAR(128) NULL COMMENT '创建人', create_time datetime NULL COMMENT '创建时间', modifier VARCHAR(128) NULL COMMENT '修改人', modify_time datetime NULL COMMENT '修改时间', CONSTRAINT PK_UC_USER_INFO PRIMARY KEY (id)) COMMENT='用户信息';
+--changeset Petty:1610534973313-5
+CREATE TABLE uc_user_info (id VARCHAR(128) DEFAULT '' NOT NULL, user_id VARCHAR(128) NOT NULL COMMENT '用户ID', user_name VARCHAR(128) NULL COMMENT '姓名', nick_name VARCHAR(128) NULL COMMENT '用户昵称', user_sex TINYINT(3) NULL COMMENT '性别', user_born datetime NULL COMMENT '生日', user_avatar VARCHAR(1000) NULL COMMENT '用户头像', email VARCHAR(128) NULL COMMENT '电子邮件', user_address VARCHAR(400) NULL COMMENT '居住地址', mobile_tel VARCHAR(128) NULL COMMENT '移动电话', phone_tel VARCHAR(96) NULL COMMENT '用户联系电话', user_iden_type VARCHAR(64) NULL COMMENT '用户证件类型', user_iden VARCHAR(128) NULL COMMENT '证件ID', `organization` VARCHAR(255) NULL COMMENT '工作单位', education VARCHAR(255) NULL COMMENT '学历', positional_title VARCHAR(255) NULL COMMENT '职称', political_status VARCHAR(64) NULL COMMENT '政治面貌', graduated_school VARCHAR(255) NULL COMMENT '毕业学校', graduated_year VARCHAR(128) NULL COMMENT '毕业年份', major VARCHAR(128) NULL COMMENT '所学专业', verified TINYINT(3) DEFAULT 0 NULL COMMENT '是否实名认证', region_id VARCHAR(96) NULL COMMENT '行政区划CODE', tenant_id VARCHAR(255) NULL COMMENT '租户ID', del_flag TINYINT(3) UNSIGNED DEFAULT 0 NULL COMMENT '删除标记 0 未删除 1 删除', creator VARCHAR(128) NULL COMMENT '创建人', create_time datetime NULL COMMENT '创建时间', modifier VARCHAR(128) NULL COMMENT '修改人', modify_time datetime NULL COMMENT '修改时间', CONSTRAINT PK_UC_USER_INFO PRIMARY KEY (id)) COMMENT='用户信息';
 ALTER TABLE uc_user_info COMMENT = '用户信息';
 
---changeset Petty:1610268206696-6
+--changeset Petty:1610534973313-6
 CREATE TABLE uc_user_role (id VARCHAR(128) NOT NULL, user_id VARCHAR(128) NOT NULL, role_id VARCHAR(128) NOT NULL, del_flag TINYINT(3) UNSIGNED DEFAULT 0 NULL COMMENT '删除标记 0 未删除 1 删除', creator VARCHAR(128) NULL COMMENT '创建人', create_time datetime NULL COMMENT '创建时间', modifier VARCHAR(128) NULL COMMENT '修改人', modify_time datetime NULL COMMENT '修改时间', CONSTRAINT PK_UC_USER_ROLE PRIMARY KEY (id)) COMMENT='用户角色关联信息';
 ALTER TABLE uc_user_role COMMENT = '用户角色关联信息';
 
---changeset Petty:1610268206696-7
+--changeset Petty:1610534973313-7
 ALTER TABLE uc_auth_client_details ADD CONSTRAINT client_id_del_flag_unique UNIQUE (client_id, del_flag);
 
---changeset Petty:1610268206696-8
+--changeset Petty:1610534973313-8
 ALTER TABLE uc_user ADD CONSTRAINT login_ld_del_flag_unique UNIQUE (login_id, del_flag);
 
---changeset Petty:1610268206696-9
+--changeset Petty:1610534973313-9
 ALTER TABLE uc_user ADD CONSTRAINT open_id_union_id_del_flag_unique UNIQUE (open_id, union_id, del_flag);
 
---changeset Petty:1610268206696-10
+--changeset Petty:1610534973313-10
 ALTER TABLE uc_role ADD CONSTRAINT role_del_flag_unique UNIQUE (`role`, del_flag);
 
---changeset Petty:1610268206696-11
+--changeset Petty:1610534973313-11
 ALTER TABLE uc_tenant ADD CONSTRAINT tenant_id_del_flag_unique UNIQUE (tenant_id, del_flag);
 
---changeset Petty:1610268206696-12
+--changeset Petty:1610534973313-12
 ALTER TABLE uc_user_info ADD CONSTRAINT user_id_del_flag_unique UNIQUE (user_id, user_iden, del_flag);
 
---changeset Petty:1610268206696-13
+--changeset Petty:1610534973313-13
 CREATE INDEX role_id_index ON uc_user_role(role_id);
 
---changeset Petty:1610268206696-14
+--changeset Petty:1610534973313-14
 CREATE INDEX user_id_index ON uc_user_role(user_id);
+
+--changeset Petty:1610534973313-15
+CREATE VIEW v_user_info_view AS select `user`.`id` AS `id`,`user`.`tenant_id` AS `tenant_id`,`user`.`user_channel` AS `user_channel`,`user`.`status` AS `status`,`user_info`.`user_name` AS `user_name`,`user_info`.`nick_name` AS `nick_name`,`user_info`.`user_sex` AS `user_sex`,`user_info`.`user_born` AS `user_born`,`user_info`.`user_avatar` AS `user_avatar`,`user_info`.`email` AS `email`,`user_info`.`user_address` AS `user_address`,`user_info`.`mobile_tel` AS `mobile_tel`,`user_info`.`phone_tel` AS `phone_tel`,`user_info`.`user_iden_type` AS `user_iden_type`,`user_info`.`user_iden` AS `user_iden`,`user_info`.`organization` AS `organization`,`user_info`.`education` AS `education`,`user_info`.`positional_title` AS `positional_title`,`user_info`.`political_status` AS `political_status`,`user_info`.`graduated_school` AS `graduated_school`,`user_info`.`graduated_year` AS `graduated_year`,`user_info`.`major` AS `major`,`user_info`.`verified` AS `verified`,`user_info`.`region_id` AS `region_id`,`user_info`.`creator` AS `creator`,(select ifnull(`u`.`user_name`,`u`.`nick_name`) from `bali_user_center`.`uc_user_info` `u` where (`user_info`.`creator` = `u`.`user_id`)) AS `creator_name`,`user_info`.`create_time` AS `create_time`,`user_info`.`modifier` AS `modifier`,(select ifnull(`u`.`user_name`,`u`.`nick_name`) from `bali_user_center`.`uc_user_info` `u` where (`user_info`.`modifier` = `u`.`user_id`)) AS `modifier_name`,`user_info`.`modify_time` AS `modify_time` from (`bali_user_center`.`uc_user` `user` left join `bali_user_center`.`uc_user_info` `user_info` on((`user`.`id` = `user_info`.`user_id`))) where (`user`.`del_flag` = 0);
 
--- bali-auth-server/src/main/resources/db/changelog/init/changelog-init-dml.mysql.sql
+++ bali-auth-server/src/main/resources/db/changelog/init/changelog-init-dml.mysql.sql
@@ -1,14 +1,21 @@
 --liquibase formatted sql
 
---changeset Petty:1610264051415-1
-INSERT INTO uc_auth_client_details (id, client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, auto_approve, tenant_id, del_flag, creator, create_time, modifier, modify_time) VALUES ('1347517937222574085', 'bali', NULL, '{noop}123456', 'user.read,resource.read', 'password,refresh_token,authorization_code,client_credentials,mobile,wechat', 'http://localhost:9001/login/oauth2/code/bali', NULL, 36000, 36000, NULL, NULL, '0000000001', 0, NULL, '2021-01-08 21:26:13', NULL, '2021-01-08 21:26:18');
+--changeset Petty:1610535142257-1
+INSERT INTO uc_auth_client_details (id, client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, auto_approve, del_flag, creator, create_time, modifier, modify_time, tenant_id) VALUES ('0000000000000000001', 'bali', NULL, '{noop}123456', 'user.read,resource.read', 'password,refresh_token,authorization_code,client_credentials,mobile,wechat', 'http://localhost:9001/login/oauth2/code/bali', NULL, 36000, 36000, NULL, NULL, 0, NULL, '2021-01-08 21:26:13', NULL, '2021-01-08 21:26:18', '0000000001');
 
---changeset Petty:1610264051415-2
-INSERT INTO uc_tenant (id, tenant_id, tenant_name, link_man, contact_number, address, remark, email, status, del_flag, creator, create_time, modifier, modify_time) VALUES ('1250754808025112580', '0000000001', '系统管理方', 'Alex Pettyfer', '13094186549', '广州', NULL, 'pettyferlove@live.cn', 1, 0, '1250752809141460993', '2019-07-13 11:58:05', '1250752809141460993', '2020-04-16 20:08:56');
+--changeset Petty:1610535142257-2
+INSERT INTO uc_role (id, `role`, role_name, `description`, sort, status, tenant_id, del_flag, creator, create_time, modifier, modify_time) VALUES ('0000000000000000001', 'SUPER_ADMIN', '超级管理员', '系统超级管理员，只允许进行租户操作等一系列系统设置', 99, 1, '0000000001', 0, '0000000000000000001', '2021-01-13 09:06:30', '0000000000000000001', '2021-01-13 10:56:14');
+INSERT INTO uc_role (id, `role`, role_name, `description`, sort, status, tenant_id, del_flag, creator, create_time, modifier, modify_time) VALUES ('0000000000000000002', 'TENANT_ADMIN', '租户管理员', '租户的管理员，可用于控制租户下的数据，改角色不归属于任何租户，属于全局共享角色，只能由超级管理员进行统一维护', 98, 1, NULL, 0, '0000000000000000001', '2021-01-13 10:55:45', '0000000000000000001', '2021-01-13 10:57:54');
 
---changeset Petty:1610264051415-3
-INSERT INTO uc_user (id, login_id, password, tenant_id, open_id, union_id, user_channel, status, del_flag, creator, create_time, modifier, modify_time) VALUES ('1347774994585444353', 'admin', '{bcrypt}$2a$10$GX3zqMtiaO1ywII/XxAj/eJI8j9xyxAGr8PgcxWbSkuz.v2CfSBte', '0000000001', NULL, NULL, 'web', 1, 0, NULL, NULL, NULL, NULL);
+--changeset Petty:1610535142257-3
+INSERT INTO uc_tenant (id, tenant_id, tenant_name, link_man, contact_number, address, remark, email, status, del_flag, creator, create_time, modifier, modify_time) VALUES ('0000000000000000001', '0000000001', '系统管理方', 'Alex Pettyfer', '13094186549', '广州', '', 'pettyferlove@live.cn', 1, 0, '0000000000000000001', '2019-07-13 11:58:05', '0000000000000000001', '2021-01-13 09:55:21');
 
---changeset Petty:1610264051415-4
-INSERT INTO uc_user_info (id, user_id, user_name, nick_name, user_sex, user_born, user_avatar, email, user_address, mobile_tel, phone_tel, user_iden_type, user_iden, `organization`, education, positional_title, political_status, graduated_school, graduated_year, major, verified, region_id, tenant_id, del_flag, creator, create_time, modifier, modify_time) VALUES ('1348169137282019330', '1347774994585444353', '超级管理员', NULL, NULL, NULL, 'https://bali-attachment.oss-cn-shanghai.aliyuncs.com/bali/avatar/430ae478c2e74ee1b563caca5fbf0349.png', 'pettyferlove@live.cn', NULL, '13094186549', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, '0000000001', 0, NULL, NULL, NULL, NULL);
+--changeset Petty:1610535142257-4
+INSERT INTO uc_user (id, login_id, password, tenant_id, open_id, union_id, user_channel, status, del_flag, creator, create_time, modifier, modify_time) VALUES ('0000000000000000001', 'administrator', '{bcrypt}$2a$10$GX3zqMtiaO1ywII/XxAj/eJI8j9xyxAGr8PgcxWbSkuz.v2CfSBte', '0000000001', NULL, NULL, 'web', 1, 0, '0000000000000000001', NULL, '0000000000000000001', '2021-01-13 15:32:28');
+
+--changeset Petty:1610535142257-5
+INSERT INTO uc_user_info (id, user_id, user_name, nick_name, user_sex, user_born, user_avatar, email, user_address, mobile_tel, phone_tel, user_iden_type, user_iden, `organization`, education, positional_title, political_status, graduated_school, graduated_year, major, verified, region_id, tenant_id, del_flag, creator, create_time, modifier, modify_time) VALUES ('0000000000000000001', '0000000000000000001', '超级管理员', '超级管理员', NULL, '2020-12-08 00:00:00', 'https://bali-attachment.oss-cn-shanghai.aliyuncs.com/bali/avatar/430ae478c2e74ee1b563caca5fbf0349.png', 'pettyferlove@live.cn', '', '13094186549', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, '0000000001', 0, '0000000000000000001', NULL, '0000000000000000001', '2021-01-13 15:32:28');
+
+--changeset Petty:1610535142257-6
+INSERT INTO uc_user_role (id, user_id, role_id, del_flag, creator, create_time, modifier, modify_time) VALUES ('0000000000000000001', '0000000000000000001', '0000000000000000001', 0, '0000000000000000001', NULL, NULL, NULL);
 
