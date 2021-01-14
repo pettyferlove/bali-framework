4d946ecf14717e0ee0728f245fb23e1b21065634
Pettyfer <pettyferlove@live.cn>
2021-01-14 13:56:15 +0800

    用户中心

--- bali-auth-server/src/main/resources/static/js/pages/tenant/index.js
+++ bali-auth-server/src/main/resources/static/js/pages/tenant/index.js
@@ -1,14 +1,15 @@
-layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'element', 'slider'], function () {
-    let laydate = layui.laydate
-        , layer = layui.layer //弹层
+layui.use(['layer', 'table'], function () {
+    let layer = layui.layer
         , table = layui.table
 
-    //执行一个 table 实例
+    let module = '/tenant';
+    let moduleName = '租户';
+
     table.render({
         elem: '#table'
         , height: 420
-        , url: '/tenant/list' //数据接口
-        , title: '租户表'
+        , url: module + '/list' //数据接口
+        , title: moduleName + '列表'
         , page: true //开启分页
         , toolbar: 'default' //开启工具栏，此处显示默认图标，可以自定义模板，详见文档
         , cols: [[ //表头
@@ -63,27 +64,12 @@ layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'elemen
         active[type] ? active[type].call(this) : '';
     });
 
-    //监听头工具栏事件
     table.on('toolbar(filter)', function (obj) {
         let checkStatus = table.checkStatus(obj.config.id)
             , data = checkStatus.data; //获取选中的数据
         switch (obj.event) {
             case 'add':
-                layer.open({
-                    type: 2,
-                    title: '添加租户',
-                    btn: ['创建', '取消'],
-                    area: ['50%', '70%'],
-                    content: '/tenant/add'
-                    , yes: function (index, layero) {
-                        let submit = layero.find('iframe').contents().find("#form-submit");
-                        submit.click();
-                    }
-                    , btn2: function (index) {
-                        layer.close(index)
-                        return false;
-                    }
-                });
+                add();
                 break;
             case 'update':
                 if (data.length === 0) {
@@ -91,93 +77,83 @@ layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'elemen
                 } else if (data.length > 1) {
                     layer.msg('只能同时编辑一个');
                 } else {
-                    layer.open({
-                        type: 2,
-                        title: '编辑租户',
-                        btn: ['保存', '取消'],
-                        area: ['50%', '70%'],
-                        content: '/tenant/edit/' + checkStatus.data[0].id
-                        , yes: function (index, layero) {
-                            let submit = layero.find('iframe').contents().find("#form-submit");
-                            submit.click();
-                        }
-                        , btn2: function (index) {
-                            layer.close(index)
-                            return false;
-                        }
-                    });
+                    edit(checkStatus.data[0].id);
                 }
                 break;
             case 'delete':
                 if (data.length === 0) {
                     layer.msg('请选择一行');
                 } else {
-                    layer.confirm('真的删除行么', function (index) {
-                        $.ajax({
-                            type: "DELETE",
-                            url: "/tenant/delete/" + checkStatus.data[0].id,
-                            success: function (res) {
-                                if(res.message){
-                                    layer.msg(res.message, {icon: 2});
-                                } else {
-                                    layer.msg('删除成功');
-                                    table.reload('table')
-                                }
-                            },
-                            error: function (err) {
-                                layer.msg('删除失败', {icon: 2});
-                            }
-                        })
-                        layer.close(index);
-                    });
+                    del(checkStatus.data[0].id);
                 }
                 break;
         }
         ;
     });
 
-    //监听行工具事件
     table.on('tool(filter)', function (obj) {
         let data = obj.data
             , layEvent = obj.event;
         if (layEvent === 'detail') {
             layer.msg('查看操作');
         } else if (layEvent === 'del') {
-            layer.confirm('真的删除行么', function (index) {
-                $.ajax({
-                    type: "DELETE",
-                    url: "/tenant/delete/" + data.id,
-                    success: function (res) {
-                        if(res.message){
-                            layer.msg(res.message, {icon: 2});
-                        } else {
-                            layer.msg('删除成功');
-                            table.reload('table')
-                        }
-                    },
-                    error: function (err) {
-                        layer.msg('删除失败', {icon: 2});
-                    }
-                })
-                layer.close(index);
-            });
+            del(data.id);
         } else if (layEvent === 'edit') {
-            layer.open({
-                type: 2,
-                title: '编辑租户',
-                btn: ['保存', '取消'],
-                area: ['50%', '70%'],
-                content: '/tenant/edit/' + data.id
-                , yes: function (index, layero) {
-                    let submit = layero.find('iframe').contents().find("#form-submit");
-                    submit.click();
-                }
-                , btn2: function (index) {
-                    layer.close(index)
-                    return false;
-                }
-            });
+            edit(data.id);
         }
     });
+
+    function add(){
+        layer.open({
+            type: 2,
+            title: '添加' + moduleName,
+            btn: ['创建', '取消'],
+            area: ['50%', '70%'],
+            content: module + '/add'
+            , yes: function (index, layero) {
+                let submit = layero.find('iframe').contents().find("#form-submit");
+                submit.click();
+            }
+            , btn2: function (index) {
+                layer.close(index)
+                return false;
+            }
+        });
+    }
+
+    function del(id){
+        layer.confirm('真的删除行么', function (index) {
+            $.ajax({
+                type: "DELETE",
+                url: module + '/delete/' + id,
+                success: function (res) {
+                    layer.msg('删除成功');
+                    table.reload('table')
+                },
+                error: function (err) {
+                    layer.msg('删除失败', {icon: 2});
+                }
+            })
+            layer.close(index);
+        });
+    }
+
+    function edit(id) {
+        layer.open({
+            type: 2,
+            title: '编辑' + moduleName,
+            btn: ['保存', '取消'],
+            area: ['50%', '70%'],
+            content: module + '/edit/' + id
+            , yes: function (index, layero) {
+                let submit = layero.find('iframe').contents().find("#form-submit");
+                submit.click();
+            }
+            , btn2: function (index) {
+                layer.close(index)
+                return false;
+            }
+        });
+    }
 
 });
--- bali-auth-server/src/main/resources/static/js/pages/user/index.js
+++ bali-auth-server/src/main/resources/static/js/pages/user/index.js
@@ -1,14 +1,15 @@
-layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'element', 'slider'], function () {
-    let laydate = layui.laydate
-        , layer = layui.layer //弹层
+layui.use(['layer', 'table'], function () {
+    let layer = layui.layer
         , table = layui.table
 
-    //执行一个 table 实例
+    let module = '/user';
+    let moduleName = '用户';
+
     table.render({
         elem: '#table'
         , height: 420
-        , url: '/user/list' //数据接口
-        , title: '用户表'
+        , url: module + '/list' //数据接口
+        , title: moduleName + '列表'
         , page: true //开启分页
         , toolbar: 'default' //开启工具栏，此处显示默认图标，可以自定义模板，详见文档
         , cols: [[ //表头
@@ -64,27 +65,13 @@ layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'elemen
         active[type] ? active[type].call(this) : '';
     });
 
-    //监听头工具栏事件
+
     table.on('toolbar(filter)', function (obj) {
         let checkStatus = table.checkStatus(obj.config.id)
             , data = checkStatus.data; //获取选中的数据
         switch (obj.event) {
             case 'add':
-                layer.open({
-                    type: 2,
-                    title: '添加用户',
-                    btn: ['创建', '取消'],
-                    area: ['70%', '70%'],
-                    content: '/user/add'
-                    , yes: function (index, layero) {
-                        let submit = layero.find('iframe').contents().find("#form-submit");
-                        submit.click();
-                    }
-                    , btn2: function (index) {
-                        layer.close(index)
-                        return false;
-                    }
-                });
+                add();
                 break;
             case 'update':
                 if (data.length === 0) {
@@ -92,101 +79,87 @@ layui.use(['laydate', 'laypage', 'layer', 'table', 'carousel', 'upload', 'elemen
                 } else if (data.length > 1) {
                     layer.msg('只能同时编辑一个');
                 } else {
-                    if (checkStatus.data[0].userChannel === 'web') {
-                        layer.open({
-                            type: 2,
-                            title: '编辑用户',
-                            btn: ['保存', '取消'],
-                            area: ['70%', '70%'],
-                            content: '/user/edit/' + checkStatus.data[0].id
-                            , yes: function (index, layero) {
-                                let submit = layero.find('iframe').contents().find("#form-submit");
-                                submit.click();
-                            }
-                            , btn2: function (index) {
-                                layer.close(index)
-                                return false;
-                            }
-                        });
-                    } else {
-                        layer.msg('不支持在Web端修改移动端用户信息');
-                    }
+                    edit(checkStatus.data[0].id, checkStatus.data[0].userChannel);
                 }
                 break;
             case 'delete':
                 if (data.length === 0) {
                     layer.msg('请选择一行');
                 } else {
-                    layer.confirm('真的删除行么', function (index) {
-                        $.ajax({
-                            type: "DELETE",
-                            url: "/user/delete/" + checkStatus.data[0].id,
-                            success: function (res) {
-                                if(res.message){
-                                    layer.msg(res.message, {icon: 2});
-                                } else {
-                                    layer.msg('删除成功');
-                                    table.reload('table')
-                                }
-                            },
-                            error: function (err) {
-                                layer.msg('删除失败', {icon: 2});
-                            }
-                        })
-                        layer.close(index);
-                    });
+                    del(checkStatus.data[0].id);
                 }
                 break;
         }
         ;
     });
 
-    //监听行工具事件
     table.on('tool(filter)', function (obj) {
         let data = obj.data
             , layEvent = obj.event;
         if (layEvent === 'detail') {
             layer.msg('查看操作');
         } else if (layEvent === 'del') {
-            layer.confirm('真的删除行么', function (index) {
-                $.ajax({
-                    type: "DELETE",
-                    url: "/user/delete/" + data.id,
-                    success: function (res) {
-                        if(res.message){
-                            layer.msg(res.message, {icon: 2});
-                        } else {
-                            layer.msg('删除成功');
-                            table.reload('table')
-                        }
-                    },
-                    error: function (err) {
-                        layer.msg('删除失败', {icon: 2});
-                    }
-                })
-                layer.close(index);
-            });
+            del(data.id);
         } else if (layEvent === 'edit') {
-            if (data.userChannel === 'web') {
-                layer.open({
-                    type: 2,
-                    title: '编辑用户',
-                    btn: ['保存', '取消'],
-                    area: ['70%', '70%'],
-                    content: '/user/edit/' + data.id
-                    , yes: function (index, layero) {
-                        let submit = layero.find('iframe').contents().find("#form-submit");
-                        submit.click();
-                    }
-                    , btn2: function (index) {
-                        layer.close(index)
-                        return false;
-                    }
-                });
-            } else {
-                layer.msg('不支持在Web端修改移动端用户信息');
-            }
+            edit(data.id, data.userChannel);
         }
     });
+
+    function add(){
+        layer.open({
+            type: 2,
+            title: '添加' + moduleName,
+            btn: ['创建', '取消'],
+            area: ['70%', '70%'],
+            content: module + '/add'
+            , yes: function (index, layero) {
+                let submit = layero.find('iframe').contents().find("#form-submit");
+                submit.click();
+            }
+            , btn2: function (index) {
+                layer.close(index)
+                return false;
+            }
+        });
+    }
+
+    function del(id){
+        layer.confirm('真的删除行么', function (index) {
+            $.ajax({
+                type: "DELETE",
+                url: module + '/delete/' + id,
+                success: function (res) {
+                    layer.msg('删除成功');
+                    table.reload('table')
+                },
+                error: function (err) {
+                    layer.msg('删除失败', {icon: 2});
+                }
+            })
+            layer.close(index);
+        });
+    }
+
+    function edit(id, type) {
+        if (type === 'web') {
+            layer.open({
+                type: 2,
+                title: '编辑' + moduleName,
+                btn: ['保存', '取消'],
+                area: ['70%', '70%'],
+                content: module + '/edit/' + id
+                , yes: function (index, layero) {
+                    let submit = layero.find('iframe').contents().find("#form-submit");
+                    submit.click();
+                }
+                , btn2: function (index) {
+                    layer.close(index)
+                    return false;
+                }
+            });
+        } else {
+            layer.msg('不支持在Web端修改移动端用户信息');
+        }
+    }
 
 });
