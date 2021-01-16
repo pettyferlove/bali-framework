4d8e678c908e620dcdacee59c6a795e702bc4b35
Alex Pettyfer <pettyferlove@live.cn>
2021-01-16 09:36:01 +0800

    :tada: Initial commit.

--- bali-auth-server/src/main/java/com/github/bali/auth/controller/ResourceController.java
+++ bali-auth-server/src/main/java/com/github/bali/auth/controller/ResourceController.java
@@ -1,27 +1,39 @@
 package com.github.bali.auth.controller;
 
 import com.github.bali.auth.domain.vo.UserDetails;
+import com.github.bali.auth.entity.Role;
+import com.github.bali.auth.service.IRoleService;
 import com.github.bali.security.userdetails.BaliUserDetails;
 import com.github.bali.security.utils.SecurityUtil;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Petty
  */
 @RestController
 @RequestMapping("/resource")
-@PreAuthorize("#oauth2.hasAnyScope('resource.read','user.read')")
 public class ResourceController {
 
+
+    private final IRoleService roleService;
+
+    public ResourceController(IRoleService roleService) {
+        this.roleService = roleService;
+    }
 
     /**
      * 第三方客户端通过获取用户详细信息
      *
      * @return UserDetails
      */
-    @RequestMapping("user-info")
+
+    @PreAuthorize("hasRole('TENANT_ADMIN')&&#oauth2.hasScope('user.read')")
+    @GetMapping("user-info")
     public UserDetails userInfo() {
         BaliUserDetails user = SecurityUtil.getUser();
         return UserDetails.builder()
@@ -38,6 +50,12 @@ public class ResourceController {
                 .accountNonExpired(user.isAccountNonExpired())
                 .credentialsNonExpired(user.isCredentialsNonExpired())
                 .build();
+    }
+
+    @PreAuthorize("hasRole('TENANT_ADMIN')&&#oauth2.hasScope('resource.read')")
+    @GetMapping("role/all")
+    public List<Role> allRole() {
+        return roleService.list();
     }
 
 
