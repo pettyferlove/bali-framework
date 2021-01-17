9e08646b99a5e57475e22416e43bb4269a6a1390
Alex Pettyfer <pettyferlove@live.cn>
2021-01-17 10:59:17 +0800

    :tada: Initial commit.

--- bali-auth-server/src/main/resources/static/js/pages/tenant/edit.js
+++ bali-auth-server/src/main/resources/static/js/pages/tenant/edit.js
@@ -1,6 +1,34 @@
 layui.use(['form'], function(){
     let form = layui.form,layer = layui.layer
-    //监听提交
+
+    form.verify({
+        contactNumber: function (value, item) { //value：表单的值、item：表单的DOM对象
+            if (value && value.length > 0) {
+                if (
+                    !/^[1][3-9][0-9]{9}$/.test(value) &&
+                    !/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(value)
+                ) {
+                    if (!/^[1][3-9][0-9]{9}$/.test(value)) {
+                        return "不是有效的手机号码";
+                    } else {
+                        return "固定电话有误，请重填";
+                    }
+                }
+            } else {
+                return "联系电话是必填项，不能为空";
+            }
+        },
+        email: function (value, item) {
+            if (value && value.length > 0) {
+                if (!/^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/.test(value)) {
+                    return '不是有效的email地址';
+                }
+            } else {
+                return "请输入电子邮箱";
+            }
+        },
+    });
+
     form.on('submit(form-submit)', function(data){
         if(!data.field.status){
             data.field.status = 0
