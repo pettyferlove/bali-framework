81246252f9bd3fd49f6926f0acf09a2b9d9b0bc0
Alex Pettyfer <pettyferlove@live.cn>
2021-01-18 19:16:56 +0800

    :tada: Initial commit.

--- bali-auth-server/src/main/resources/db/changelog/0.0.1-snapshot/changelog-0.0.1-snapshot-ddl.mysql.sql
+++ bali-auth-server/src/main/resources/db/changelog/0.0.1-snapshot/changelog-0.0.1-snapshot-ddl.mysql.sql
@@ -130,3 +130,15 @@ select `uc_role`.`role`        AS `role`,
        `uc_role`.`modify_time` AS `modify_time`
 from `uc_role`;
 
+--changeset Petty:uc-0.0.1-snapshot-ddl-12
+drop view v_role_view;
+create view v_role_view as
+select `uc_role`.`role`        AS `role`,
+       `uc_role`.`role_name`   AS `role_name`,
+       `uc_role`.`description` AS `description`,
+       `uc_role`.`sort`        AS `sort`,
+       `uc_role`.`status`      AS `status`,
+       `uc_role`.`tenant_id`   AS `tenant_id`,
+       `uc_role`.`create_time` AS `create_time`,
+       `uc_role`.`modify_time` AS `modify_time`
+from `uc_role` where `del_flag` = 0;
