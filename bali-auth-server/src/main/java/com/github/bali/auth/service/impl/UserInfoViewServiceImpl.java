d934c20666dcc4d5103f8f1724164db59370b6d2
Alex Pettyfer <pettyferlove@live.cn>
2021-01-20 19:31:12 +0800

    :tada: Initial commit.

--- bali-auth-server/src/main/java/com/github/bali/auth/service/impl/UserInfoViewServiceImpl.java
+++ bali-auth-server/src/main/java/com/github/bali/auth/service/impl/UserInfoViewServiceImpl.java
@@ -10,6 +10,7 @@ import com.github.bali.auth.domain.vo.UserInfoQueryParams;
 import com.github.bali.auth.entity.UserInfoView;
 import com.github.bali.auth.mapper.UserInfoViewMapper;
 import com.github.bali.auth.service.IUserInfoViewService;
+import com.github.bali.core.framework.exception.BaseRuntimeException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,6 +28,9 @@ public class UserInfoViewServiceImpl extends ServiceImpl<UserInfoViewMapper, Use
     @Override
     public IPage<UserInfoView> page(UserInfoQueryParams params, Page<UserInfoView> page) {
         LambdaQueryWrapper<UserInfoView> queryWrapper = Wrappers.<UserInfoView>lambdaQuery();
+        if(params.getChannel() == null) {
+            throw new BaseRuntimeException("请指定用户渠道");
+        }
         queryWrapper.eq(UserInfoView::getUserChannel, params.getChannel().getValue());
         queryWrapper.likeRight(StrUtil.isNotEmpty(params.getUserName()), UserInfoView::getUserName, params.getUserName());
         queryWrapper.likeRight(StrUtil.isNotEmpty(params.getNickName()), UserInfoView::getNickName, params.getNickName());
