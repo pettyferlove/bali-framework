8adc8850f1fb15e771310b481e86a4200ca16d84
Pettyfer <pettyferlove@live.cn>
2021-01-28 17:48:18 +0800

    增加附件扩展

---
+++ bali-extension/bali-extension-attachment/src/main/java/com/github/bali/attachment/BaliExtensionAttachmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.bali.attachment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaliExtensionAttachmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaliExtensionAttachmentApplication.class, args);
+    }
+
+}
