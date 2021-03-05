30ddf9a2ab3452999148151d00f314d2eb6ea6e2
Pettyfer <pettyferlove@live.cn>
2021-03-05 16:33:44 +0800

    修改开发环境配置

--- bali-auth-server/src/main/resources/db/auth/changelog/0.4.0-snapshot/changelog-0.4.0-snapshot-dml.mysql.sql
+++ bali-auth-server/src/main/resources/db/auth/changelog/0.4.0-snapshot/changelog-0.4.0-snapshot-dml.mysql.sql
@@ -1,1 +1,7 @@
 --liquibase formatted sql
+
+--changeset Petty:uc-0.4.0-snapshot-dml-1
+insert into uc_auth_client_scope (id, scope, auto, description, del_flag, creator, create_time, modifier, modify_time, tenant_id) values ('0000000000000000002', 'resource.read', 0, '读取资源信息', 0, '0000000000000000001', '2021-03-05 20:22:12', null, null, '0000000001');
+
+insert into uc_auth_client_details_scope (id, details_id, scope_id) values ('0000000000000000002', '0000000000000000001', '0000000000000000002');
+
