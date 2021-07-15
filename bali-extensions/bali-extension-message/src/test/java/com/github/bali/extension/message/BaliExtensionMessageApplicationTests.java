1709d6547fb83087c9e82ef111d877d316b48f4b
Pettyfer <pettyferlove@live.cn>
2021-07-15 14:00:14 +0800

    增加消息扩展

---
+++ bali-extensions/bali-extension-message/src/test/java/com/github/bali/extension/message/BaliExtensionMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bali.extension.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaliExtensionMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
