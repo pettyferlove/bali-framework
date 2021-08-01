b094dc8f2427eba1bd5dd2ef7c4bdd0f97646f53
pettyfer <pettyferlove@live.cn>
2021-08-01 08:46:09 +0800

    :wrench: 修改Docx文件名后缀

--- bali-auth-server/src/main/resources/db/auth/changelog/1.0.1-release/changelog-1.0.1-release-ddl.mysql.sql
+++ bali-auth-server/src/main/resources/db/auth/changelog/1.0.1-release/changelog-1.0.1-release-ddl.mysql.sql
@@ -2,3 +2,6 @@
 
 --changeset Petty:uc-1.0.1-release-ddl-1
 alter table uc_auth_client_details add client_secret_original varchar(256) null comment '客户端密钥原文' after client_secret;
+
+--changeset Petty:uc-1.0.1-release-ddl-2
+create index user_channel_index on uc_user (user_channel);
