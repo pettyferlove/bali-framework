1a18dd0af246aaeefe484529eb648737f4af0f51
Pettyfer <pettyferlove@live.cn>
2021-08-20 10:01:43 +0800

    修复部分bug

--- bali-core/bali-core-persistence-mybatis/src/main/java/com/github/bali/persistence/configuration/MultiTenancyAutoConfiguration.java
+++ bali-core/bali-core-persistence-mybatis/src/main/java/com/github/bali/persistence/configuration/MultiTenancyAutoConfiguration.java
@@ -4,7 +4,8 @@ import com.baomidou.mybatisplus.extension.plugins.tenant.TenantSqlParser;
 import com.github.bali.persistence.properties.MultiTenancyProperties;
 import com.github.bali.persistence.provider.tenant.DefaultTenantHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,16 +14,13 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
-@ConditionalOnMissingBean(MultiTenancyProperties.class)
 public class MultiTenancyAutoConfiguration {
 
-    private final MultiTenancyProperties multiTenancyProperties;
-
-    public MultiTenancyAutoConfiguration(MultiTenancyProperties multiTenancyProperties) {
-        this.multiTenancyProperties = multiTenancyProperties;
-    }
+    @Autowired
+    private MultiTenancyProperties multiTenancyProperties;
 
     @Bean
+    @ConditionalOnBean(MultiTenancyProperties.class)
     public TenantSqlParser tenantSqlParser() {
         return new TenantSqlParser()
                 .setTenantHandler(new DefaultTenantHandler(multiTenancyProperties) {
