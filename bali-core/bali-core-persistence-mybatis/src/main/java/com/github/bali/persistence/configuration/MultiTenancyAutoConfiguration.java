48e8510a55213968b0d935b0e670e4713e5df4fc
Pettyfer <pettyferlove@live.cn>
2021-08-20 10:09:12 +0800

    修复部分bug

--- bali-core/bali-core-persistence-mybatis/src/main/java/com/github/bali/persistence/configuration/MultiTenancyAutoConfiguration.java
+++ bali-core/bali-core-persistence-mybatis/src/main/java/com/github/bali/persistence/configuration/MultiTenancyAutoConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
+@SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
 public class MultiTenancyAutoConfiguration {
 
     @Autowired
