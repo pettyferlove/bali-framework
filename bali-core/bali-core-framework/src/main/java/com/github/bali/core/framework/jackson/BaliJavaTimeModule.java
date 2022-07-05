6adfe68012898eae7ad66e4285be166d6ea210e8
Alex Pettyfer <pettyferlove@live.cn>
2022-07-05 09:30:12 +0800

    增加Gson自动装配

--- bali-core/bali-core-framework/src/main/java/com/github/bali/core/framework/jackson/BaliJavaTimeModule.java
+++ bali-core/bali-core-framework/src/main/java/com/github/bali/core/framework/jackson/BaliJavaTimeModule.java
@@ -3,16 +3,13 @@ package com.github.bali.core.framework.jackson;
 import cn.hutool.core.date.DatePattern;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -30,5 +27,11 @@ public class BaliJavaTimeModule extends SimpleModule {
 		this.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
 		this.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN)));
 		this.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN)));
+		this.addSerializer(Instant.class, InstantSerializer.INSTANCE);
+		this.addSerializer(OffsetDateTime.class, OffsetDateTimeSerializer.INSTANCE);
+		this.addSerializer(ZonedDateTime.class, ZonedDateTimeSerializer.INSTANCE);
+		this.addDeserializer(Instant.class, InstantDeserializer.INSTANT);
+		this.addDeserializer(OffsetDateTime.class, InstantDeserializer.OFFSET_DATE_TIME);
+		this.addDeserializer(ZonedDateTime.class, InstantDeserializer.ZONED_DATE_TIME);
 	}
 }
